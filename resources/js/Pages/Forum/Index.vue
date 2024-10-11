990e1a51a8f8b53e442bc5f53abb8a10b6a3ae85
tamimi6030 <tamimi6030@gmail.com>
2024-10-11 14:03:24 +0300

    creating and listing topics

--- resources/js/Pages/Forum/Index.vue
+++ resources/js/Pages/Forum/Index.vue
@@ -1,6 +1,9 @@
 <script setup>
 import ForumLayout from '@/Layouts/ForumLayout.vue';
+import Select from "@/Components/Select.vue";
 import { Head } from '@inertiajs/vue3';
+import InputLabel from "@/Components/InputLabel.vue";
+
 </script>
 
 <template>
@@ -8,9 +11,22 @@ import { Head } from '@inertiajs/vue3';
 
     <ForumLayout>
 
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900"></div>
-                </div>
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900">
+
+                <InputLabel for="topic" value="Topic" class="sr-only" />
+
+
+
+                <Select>
+                    <option value="">All Topics</option>
+                    <option :value="topic.slug" v-for="topic in $page.props.topics" :key="topic.id">
+                                {{topic.name}}
+                    </option>
+
+                </Select>
+            </div>
+        </div>
 
         <template #side>
 
