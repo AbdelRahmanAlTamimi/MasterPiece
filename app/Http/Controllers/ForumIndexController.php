e943fd9c3613e1dce6c52220d938bf5656744238
tamimi6030 <tamimi6030@gmail.com>
2024-10-11 19:13:50 +0300

    tackling pagination

--- app/Http/Controllers/ForumIndexController.php
+++ app/Http/Controllers/ForumIndexController.php
@@ -16,7 +16,7 @@ class ForumIndexController extends Controller
              Discussion::with('topic')
                 ->orderByPinned()
                  ->latest()
-                 ->paginate(10)
+                 ->paginate(1)
          )
 
     ]);
--- resources/js/Pages/Forum/Index.vue
+++ resources/js/Pages/Forum/Index.vue
@@ -4,6 +4,7 @@ import Select from "@/Components/Select.vue";
 import { Head } from '@inertiajs/vue3';
 import InputLabel from "@/Components/InputLabel.vue";
 import Discussion from "@/Components/Forum/Discussion.vue";
+import Pagination from "@/Components/Pagination.vue";
 
 
 defineProps({
@@ -36,8 +37,12 @@ defineProps({
             </div>
 
             <div class="space-y-3">
-                <Discussion v-for="discussion in discussions.data" :key="discussions.id" :discussion="discussion"/>
+                <template v-if="discussions.data.length">
+                    <Discussion v-for="discussion in discussions.data" :key="discussions.id" :discussion="discussion"/>
 
+                    <Pagination :pagination="discussions.meta"/>
+
+                </template>
             </div>
 
         </div>
