c1dd3c83966b148c6b6205fa22e08ff5bac5545e
tamimi6030 <tamimi6030@gmail.com>
2024-10-23 19:21:16 +0300

    Adding the topic filter

--- resources/js/Pages/Forum/Index.vue
+++ resources/js/Pages/Forum/Index.vue
@@ -1,63 +1,63 @@
 <script setup>
 import ForumLayout from '@/Layouts/ForumLayout.vue';
-import Select from "@/Components/Select.vue";
-import { Head } from '@inertiajs/vue3';
-import InputLabel from "@/Components/InputLabel.vue";
-import Discussion from "@/Components/Forum/Discussion.vue";
-import Pagination from "@/Components/Pagination.vue";
+import Select from '@/Components/Select.vue';
+import Pagination from '@/Components/Pagination.vue';
 import Navigation from '@/Components/Forum/Navigation.vue';
-
-
+import Discussion from '@/Components/Forum/Discussion.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import { Head, router } from '@inertiajs/vue3';
+import _omitBy from 'lodash.omitby'
+import _isEmpty from 'lodash.isempty'
 
 defineProps({
     discussions: Object,
     query: Object
 })
 
+const filterTopic = (e) => {
+    router.visit('/', {
+        data: _omitBy({
+            'filter[topic]': e.target.value
+        }, _isEmpty),
+        preserveScroll: true
+    })
+}
 </script>
 
 <template>
     <Head title="Forum" />
 
     <ForumLayout>
-
-        <div class="space-y-8">
+        <div class="space-y-6">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-
-                    <InputLabel for="topic" value="Topic" class="sr-only" />
-
-
-
-                    <Select>
-                        <option value="">All Topics</option>
-                        <option :value="topic.slug" v-for="topic in $page.props.topics" :key="topic.id">
-                            {{topic.title}}
-                        </option>
-
-                    </Select>
+                    <div>
+                        <InputLabel for="topic" value="Topic" class="sr-only" />
+                        <Select id="topic" v-on:change="filterTopic">
+                            <option value="">All topics</option>
+                            <option
+                                :value="topic.slug"
+                                v-for="topic in $page.props.topics"
+                                :key="topic.id"
+                                :selected="query.filter?.topic === topic.slug"
+                            >
+                                {{ topic.title }}
+                            </option>
+                        </Select>
+                    </div>
                 </div>
             </div>
 
             <div class="space-y-3">
                 <template v-if="discussions.data.length">
-                    <Discussion v-for="discussion in discussions.data" :key="discussions.id" :discussion="discussion"/>
-
-                    <Pagination :pagination="discussions.meta"/>
-
+                    <Discussion v-for="discussion in discussions.data" :key="discussion.id" :discussion="discussion" />
+                    <Pagination class="!mt-6" :pagination="discussions.meta" />
                 </template>
             </div>
-
         </div>
 
-
-
         <template #side>
-
             <Navigation :query="query" />
-
         </template>
-
-
     </ForumLayout>
 </template>
