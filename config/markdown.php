8b51ddef5174db0ce7581edc94308df01b5dbf5e
tamimi6030 <tamimi6030@gmail.com>
2024-10-25 20:28:19 +0300

    fix markdown bugs

--- config/markdown.php
+++ config/markdown.php
@@ -1,5 +1,4 @@
 <?php
-
 return [
     'code_highlighting' => [
         /*
--- resources/js/Composables/useCreateDiscussion.js
+++ resources/js/Composables/useCreateDiscussion.js
@@ -1,14 +1,14 @@
 import { ref } from "vue";
 import { useForm } from "@inertiajs/vue3";
 
-const visible = ref(true);
+const visible = ref(false);
 
 const form = useForm({
     topic_id: "",
     title: "",
     body: "",
 });
-
+ 
 export default () => {
     const showCreateDiscussionForm = () => {
         visible.value = true;
