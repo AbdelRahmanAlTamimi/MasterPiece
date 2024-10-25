eca62b725c3eb9ca13728c84a8dfa094971a8824
tamimi6030 <tamimi6030@gmail.com>
2024-10-25 18:05:05 +0300

    discussion validation and authorization

--- resources/js/Components/Forum/Discussion.vue
+++ resources/js/Components/Forum/Discussion.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <Link :href="route('discussion.show',discussion) " class="block bg-white overflow-hidden shadow-sm sm:rounded-lg">
+    <Link :href="route('discussions.show',discussion) " class="block bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 text-gray-900 flex items-center space-x-6">
             <div class="flex-grow">
                 <div class="flex items-center space-x-3">
@@ -21,7 +21,7 @@
                 <div class="text-sm text-gray-500 mt-3 line-clamp-1">
                     {{ discussion.post.body_preview }}
                 </div>
-                <Link :href="route('discussion.show',discussion) " class="inline-block text-sm mt-3">
+                <Link :href="route('discussions.show',discussion) " class="inline-block text-sm mt-3">
                    
                     Last post by {{ discussion.latest_post.user?.username || '[user deleted]' }}
                      <time :datetime="discussion.latest_post.created_at.datetime"
