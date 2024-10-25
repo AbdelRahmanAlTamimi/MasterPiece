ecd74b054c476c5548f7028ef0532d1e956eab4b
tamimi6030 <tamimi6030@gmail.com>
2024-10-25 17:36:10 +0300

    storing a new discussion

--- app/Models/Discussion.php
+++ app/Models/Discussion.php
@@ -4,13 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Discussion extends Model
 {
     use HasFactory;
 
-    public function scopeOrderByPinned($query){
-         $query->orderBy('pinned_at', 'desc');
+    protected $fillable = [
+        'title',
+        'slug'
+    ];
+
+    protected static function booted()
+    {
+        static::created(function ($discussion) {
+            $discussion->update(['slug' => $discussion->title]);
+        });
+    }
+
+    public function setSlugAttribute($value)
+    {
+        $this->attributes['slug'] = $this->id . '-' . Str::slug($value);
+    }
+
+    public function scopeOrderByPinned($query)
+    {
+        $query->orderBy('pinned_at', 'asc');
     }
 
     public function scopeOrderByLastPost($query)
@@ -24,38 +43,47 @@ class Discussion extends Model
         );
     }
 
-    public function isPinned(){
+    public function isPinned()
+    {
         return !is_null($this->pinned_at);
     }
 
-    public function topic(){
+    public function topic()
+    {
         return $this->belongsTo(Topic::class);
     }
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
-    public function posts() {
+    public function posts()
+    {
         return $this->hasMany(Post::class);
     }
-    
+
     public function replies()
     {
         return $this->hasMany(Post::class)
             ->whereNotNull('parent_id');
     }
 
-    public function post() {
-        return $this->hasOne(Post::class)->whereNull('parent_id');
+    public function post()
+    {
+        return $this->hasOne(Post::class)
+            ->whereNull('parent_id');
     }
 
-    public function latestPost(){
-        return $this->hasOne(Post::class)->latestOfMany();
+    public function latestPost()
+    {
+        return $this->hasOne(Post::class)
+            ->latestOfMany();
     }
 
-    public function user() {
-        return $this->belongsTo(User::class);
-    }
-
-    public function participants(){
-        return $this->hasManyThrough(User::class, Post::class,'discussion_id','id','id','user_id')->distinct();
+    public function participants()
+    {
+        return $this->hasManyThrough(User::class, Post::class, 'discussion_id', 'id', 'id', 'user_id')
+            ->distinct();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,42 @@
-    <?php
+<?php
 
-    use App\Http\Controllers\ProfileController;
-    use App\Http\Controllers\ForumIndexController;
-    use App\Http\Controllers\DiscussionShowController;
-    use Illuminate\Foundation\Application;
-    use Illuminate\Support\Facades\Route;
-    use Inertia\Inertia;
-    
+use App\Http\Controllers\DiscussionShowController;
+use App\Http\Controllers\DiscussionStoreController;
+use App\Http\Controllers\ForumIndexController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-    
-    
-    Route::get('/', function () {
-        return Inertia::render('Welcome', [
-            'canLogin' => Route::has('login'),
-            'canRegister' => Route::has('register'),
-            'laravelVersion' => Application::VERSION,
-            'phpVersion' => PHP_VERSION,
-        ]);
-    });
-    
-    Route::get('/', ForumIndexController::class)->name('home');
-    Route::get('/discussions/{discussion:slug}', DiscussionShowController::class)->name('discussion.show');
-    
-    
-    Route::middleware('auth')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    });
-    
-    
-    
-    require __DIR__.'/auth.php';
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/', ForumIndexController::class)->name('home');
+Route::get('/discussions/{discussion:slug}', DiscussionShowController::class)->name('discussions.show');
+
+Route::middleware('auth')->group(function () {
+    Route::post('/discussions', DiscussionStoreController::class)->name('discussions.store');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
