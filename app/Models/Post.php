e980752d2373b6be0026ec0d3e80e55d5f81f6c9
tamimi6030 <tamimi6030@gmail.com>
2024-10-26 18:46:15 +0300

    Creating replies to discussions

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,14 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    protected $fillable = ['body'];
 
+    protected $fillable = [
+        'body'
+    ];
 
-    public function user(){
+    public function discussion()
+    {
+        return $this->belongsTo(Discussion::class);
+    }
+
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function discussion(){
-        return $this->belongsTo(Discussion::class);
+    public function parent()
+    {
+        return $this->belongsTo(Post::class, 'parent_id');
     }
 }
