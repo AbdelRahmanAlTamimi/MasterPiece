e61d92dbf30dbb4593015171a0998dec16385605
tamimi6030 <tamimi6030@gmail.com>
2024-10-26 17:01:17 +0300

    Dealing with SVG icons

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import svgLoader from "vite-svg-loader";
 
 export default defineConfig({
     plugins: [
+        svgLoader({
+            svgo: false,
+        }),
         laravel({
-            input: 'resources/js/app.js',
+            input: "resources/js/app.js",
             refresh: true,
         }),
         vue({
