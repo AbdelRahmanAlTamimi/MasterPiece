9bdb6a2f98b7b0f5efe23c57e6cc9dd4e54c9415
Abdelrahman Altamimi <tamimi6030@gmail.com>
2024-11-22 17:43:44 +0300

    create new migration for add delete_at column

--- database/migrations/2024_10_13_125329_create_posts_table.php
+++ database/migrations/2024_10_13_125329_create_posts_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->foreignId('parent_id')->nullable()->constrained('posts');
             $table->text('body');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
